e732f0763ba8b638a9d35f320d3e39272c446ddd
dalv0911 <dalv0911@gmail.com>
2014-11-17 00:40:47 +0700

    Add check_ajax for page thumuc

--- models/check_ajax.model.php
+++ models/check_ajax.model.php
@@ -1,6 +1,22 @@
 <?php
 	class check_ajaxModel extends db{
 		private static $conn=null;
+		public function check_name_menu($name){
+			if(empty(self::$conn)){
+				self::$conn=$this->connect_pdo();
+			}
+			$sql="SELECT name FROM menu WHERE name=?";
+			$stmt=self::$conn->prepare($sql);
+
+			$stmt->bindParam(1,$name);
+			$stmt->execute();
+			$num_row=$stmt->rowCount();
+			if($num_row>0){
+				return true;
+			}else{
+				return false;
+			}
+		}
 		public function check_name_thumuc($name){
 			if(empty(self::$conn)){
 				self::$conn=$this->connect_pdo();
@@ -41,6 +57,23 @@
 			$stmt=self::$conn->prepare($sql);
 
 			$stmt->bindParam(1,$name);
+			$stmt->execute();
+			$num_row=$stmt->rowCount();
+			if($num_row>0){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		public function check_edit_name_pages($name,$id){
+			if(empty(self::$conn)){
+				self::$conn=$this->connect_pdo();
+			}
+			$sql="SELECT name FROM pages WHERE name=? AND id!=?";
+			$stmt=self::$conn->prepare($sql);
+
+			$stmt->bindParam(1,$name);
+			$stmt->bindParam(2,$id);
 			$stmt->execute();
 			$num_row=$stmt->rowCount();
 			if($num_row>0){
